97384ac1c3308b29535493a4ebc616d0ee0c9ac5
prakharsrivas55 <43438593+prakharsrivas55@users.noreply.github.com>
2021-04-18 13:28:15 +0530

    Final

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+$('.slider').slick({
+    arrows:false,
+    dots:true,
+    appendDots:'.slider-dots',
+    dotsClass:'dots',
+    autoplay: true,
+    autoplaySpeed: 1000
+});
+
+});  
