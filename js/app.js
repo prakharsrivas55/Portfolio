e5beb01c0995646f6c2e91cfd1b43a21e43da85f
prakharsrivas55 <43438593+prakharsrivas55@users.noreply.github.com>
2021-04-22 22:37:45 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,4 +8,31 @@ $('.slider').slick({
     autoplaySpeed: 1000
 });
 
+let hamberger=document.querySelector('.hamberger');
+let times= document.querySelector('.times');
+
+let mobileNav= document.querySelector('.mobile-nav');
+
+
+
+
+hamberger.addEventListener('click', function(){
+    mobileNav.classList.add('open');
+
+});    
+
+times.addEventListener('click', function(){
+     mobileNav.classList.remove('open');
+     
+});
+
+mobileNav.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+    
+});
+
+times.addEventListener('click', function(){
+
 });  
+
+});
